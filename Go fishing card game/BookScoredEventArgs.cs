716149a13ee7f7a09d4f5e5b835957ac7eded7b9
Intramint <marcinwieczorek8@gmail.com>
2026-01-12 16:13:06 +0100

    Added blank BookScored event

---
+++ Go fishing card game/BookScoredEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Go_fishing_card_game
+{
+    public class BookScoredEventArgs
+    {
+        public BookScoredEventArgs (Player player, CardValues cardValue)
+        {
+            Player = player;
+            CardValue = cardValue;
+        }
+
+        Player Player;
+        CardValues CardValue;
+    }
+}
